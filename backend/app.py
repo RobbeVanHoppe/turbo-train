09b3964755c2ee417464c3b27bfb5fcc5782c321
Robbe vanHoppe <12200496@student.pxl.be>
2023-10-03 21:31:44 +0200

    Fucking around with connection

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
+import json
+import mysql.connector
+from flask_cors import CORS, cross_origin
 
-# Create a Flask web server
 app = Flask(__name__)
 
-# Define a route and a function to handle the request
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def getMysqlConnection():
+    return mysql.connector.connect(user='testing', host='0.0.0.0', port='3306', password='testing', database='test')
 
-# Run the app if this script is executed
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
+
+@app.route('/api/getMonths', methods=['GET'])
+@cross_origin() # allow all origins all methods.
+def get_months():
+    db = getMysqlConnection()
+    print(db)
+    try:
+        sqlstr = "SELECT * from users"
+        print(sqlstr)
+        cur = db.cursor()
+        cur.execute(sqlstr)
+        output_json = cur.fetchall()
+    except Exception as e:
+        print("Error in SQL:\n", e)
+    finally:
+        db.close()
+    return jsonify(results=output_json)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
